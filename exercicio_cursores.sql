e14763b4a0a546ff8028f98fa562b479713307ab
Nicolas Yudji <nicolas.yudji@hotmail.com>
2023-09-26 03:04:31 -0300

    Exercicio 1 feito

--- exercicio_cursores.sql
+++ exercicio_cursores.sql
@@ -1,5 +1,34 @@
 --1.1 Escreva um cursor que exiba as variáveis rank e youtuber de toda tupla que tiver
 --video_count pelo menos igual a 1000 e cuja category seja igual a Sports ou Music.
+DO 
+$$
+DECLARE 
+v_youtuber VARCHAR(200);
+v_top INT;
+cur_vcount_categ CURSOR FOR
+SELECT top, youtuber
+FROM tb_top_youtubers
+WHERE video_count >= 1000
+  AND (category = 'Sports ' or category = 'Music ');
+BEGIN
+-- Abra o cursor
+OPEN cur_vcount_categ;
+-- Loop para buscar e exibir as tuplas
+FETCH NEXT FROM cur_vcount_categ INTO v_top, v_youtuber;
+WHILE FOUND
+LOOP
+    -- Exiba os valores das variáveis rank e youtuber
+    RAISE NOTICE 'Rank: %, Youtuber: %', v_top, v_youtuber;
+
+    -- Busque a próxima tupla
+    FETCH NEXT FROM cur_vcount_categ INTO v_top, v_youtuber;
+END LOOP;
+
+-- Feche o cursor
+CLOSE cur_vcount_categ;
+END;
+$$
+
 
 
 --1.2 Escreva um cursor que exibe todos os nomes dos youtubers em ordem reversa. Para tal
